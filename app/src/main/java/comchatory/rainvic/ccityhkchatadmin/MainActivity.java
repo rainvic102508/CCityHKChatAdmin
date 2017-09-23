483002e11cb76fa4bb94dc35b87d580e330e61d0
victor <rainvic102508@gmail.com>
2017-09-23 16:04:04 +0800

    - init

---
+++ app/src/main/java/comchatory/rainvic/ccityhkchatadmin/MainActivity.java
@@ -0,0 +1,13 @@
+package comchatory.rainvic.ccityhkchatadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
